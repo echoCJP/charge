5f7afacf8d54b65c3a448cd502d5d8db3f943b5e
echo <897960700@qq.com>
2018-02-03 17:50:15 +0800

    1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,6 +16,7 @@ class TestController extends Controller
     
 
     public function index(Request $request){
+        var_dump(config('cache.expired.auth'));
         return $this->init->success('hehe');
     }
 }
