07b147593955e70746108fc57f6c259278a5c200
echo <897960700@qq.com>
2018-02-10 11:11:40 +0800

    1

--- charge_xcx/pages/index/index.js
+++ charge_xcx/pages/index/index.js
@@ -5,16 +5,39 @@ const app = getApp()
 Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    user_id:'',
     sum:{}
   },
-  onLoad: function () {
-    this.getSum()
+  onLoad: function (options) {
+    
+       
+    //解决异步请求返回慢的问题,加载全局变量到this.d
+    if(app.d.userInfo){
+      this.setData({d:app.d})
+      // this.setData({user_id:app.d.userInfo.id})
+      this.getSum(app.d.userInfo.id)
+    }else{
+      app.requestReady = (res)=>{
+        // console.log(app.d.userInfo.id)
+        this.setData({d:app.d})
+        // this.setData({user_id:app.d.userInfo.id})
+        // console.log(app.d.userInfo.id)
+        this.getSum(app.d.userInfo.id)
+      }
+    }
+
+    console.log(app.d)
+    
+
+    
   },
-  getSum(){
-    app.get('/bill/counts',{user_id:1,year:2017,month:3},res=>{
+  getSum(user_id,year,month){
+    app.get('/bill/counts',{user_id:user_id,year:year,month:month},res=>{
       this.setData({sum:res})
-      // console.log(app.globalData)
     })
   },
+  getList(){
+
+  }
   
 })
